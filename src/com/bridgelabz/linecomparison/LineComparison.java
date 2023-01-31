44d7bfa437c6dad1a416d24412ffc2d23a9052fb
deepeshpradhan <dpradhan0610@gmail.com>
2023-02-01 01:17:36 +0530

    Welcome To Line Comparison Program

---
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.linecomparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Line Comparison Program");
+    }
+}
