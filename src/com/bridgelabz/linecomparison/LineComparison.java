60d6ad18ab73b5440fd85c592b66094017e54536
deepeshpradhan <dpradhan0610@gmail.com>
2023-02-01 01:58:13 +0530

    Added: Calculate the length of line using cartesian system

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -6,17 +6,17 @@ public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
 
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the value of line XY");
         System.out.println("x1 coordinate");
-        int x1 = sc.nextInt();
+        int x1 = scanner.nextInt();
         System.out.println("y1 coordinate");
-        int y1 = sc.nextInt();
+        int y1 = scanner.nextInt();
         System.out.println("x2 coordinate");
-        int x2 = sc.nextInt();
+        int x2 = scanner.nextInt();
         System.out.println("y2 coordinate");
-        int y2 = sc.nextInt();
+        int y2 = scanner.nextInt();
 
         double lineLengthXY = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
         System.out.println(" Length of the line XY is => " + lineLengthXY);
